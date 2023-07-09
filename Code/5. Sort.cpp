96a0cfec1afd15a28def34bc5159253664f4da72
Shrinidhi R Hireraddi <shrinidhirh313@gmail.com>
2023-07-09 21:41:25 +0530

    Update 5. Sort.cpp

--- Code/5. Sort.cpp
+++ Code/5. Sort.cpp
@@ -21,10 +21,10 @@ void bubbleSort(int a[], int n)
 void selectionSort(int a[], int n)
 {
     int i, j, min, temp;
-    for (i = 0; i <= n - 2; i++)
+    for (i = 0; i < n - 1; i++)
     {
         min = i;
-        for (j = i + 1; j <= n - 1; j++)
+        for (j = i + 1; j < n; j++)
             if (a[j] < a[min])
                 min = j;
         temp = a[i];
